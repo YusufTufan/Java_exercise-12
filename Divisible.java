fc77b667c166d60a288072dc8eaa3bb8be4de422
Yusuf Tufan <yusufftufan@gamil.com>
2024-04-08 15:47:47 +0300

    Update Divisible.java

--- Divisible.java
+++ Divisible.java
@@ -1,15 +1,15 @@
 public class Divisible {
     public static void main(String[] args) {
     int sayi1 = 9;
-    int sayi2 = 4;
+    int sayi2 = 18;                //2 tane değişken tanımlıyoruz.
 
-    if (divisible(sayi1, sayi2)){
+    if (divisible(sayi1, sayi2)){    //Dönüşe göre sayının durumunu yazdırıyoruz.
         System.out.println("Bölünebilir sayıdır.");
     }else{
         System.out.println("Bölünemez sayıdır.");
     }
     }
-    public static Boolean divisible (int sayi1, int sayi2){
-        return sayi1 % sayi2 ==0;
+    public static Boolean divisible (int number1, int number2){  //değişkenlerimizi buraya gönderip boolean bir dönğt alıyoruz.
+        return number1 % number2 == 0;
     }
 }
